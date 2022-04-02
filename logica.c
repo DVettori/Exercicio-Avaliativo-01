519e575ca121ad9ed23d4a627394f0cd2bf0ede8
Jucelia Vettorazzi <dnvtz@ucl.br>
2022-04-02 14:05:14 -0300

    main

---
+++ logica.c
@@ -0,0 +1,78 @@
+/* logica:
+
+1- pedir a operação e o cod binario(printf);
+
+2- receber a operação e o codigo binario(scanf);
+
+3- transformar binario pra decimal;
+
+4- fazer a operação;
+
+5- trasnformar de volta em binario;
+
+6- devolver para o usuario a resposta em binario.
+
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+//.................................................................
+
+void PedirDados(void);
+
+int ReceberDados(Void);
+
+float Transformar(int binario);
+
+float RealizarOperacao(string Operacao; float decimal);
+
+int Retransformar(float resultado);
+
+void Resultado(int resultado);
+
+//....................................................................
+
+int main(void)
+{
+    return 0;
+}
+
+//.....................................................................
+
+void PedirDados(void);
+{
+    //1- pedir a operação e o cod binario(printf);
+
+}
+
+int ReceberDados(Void);
+{
+    //2- receber a operação e o codigo binario(scanf);
+
+}
+
+float Transformar(int binario);
+{
+    //3- transformar binario pra decimal;
+
+}
+
+float RealizarOperacao(string Operacao; float decimal);
+{
+    //4- fazer a operação;
+
+}
+
+int Retransformar(float resultado);
+{
+    //5- trasnformar de volta em binario;
+
+}
+
+void Resultado(int resultado);
+{
+    //6- devolver para o usuario a resposta em binario.
+
+}
