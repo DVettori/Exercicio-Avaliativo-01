ecd18fdb5b618c707e14427227423d2fd43c88a1
Fábio <fabiojunior@ucl.br>
2022-04-05 19:46:41 -0300

    Versao Otimizada

--- exercicio/main.c
+++ exercicio/main.c
@@ -1,130 +1,93 @@
+//.....................................................................    
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+//.....................................................................        
 char operador[1];
-//...............................................................
-
-void PedirDados(void);
-
-void PedirOperador(void);
-
-int ReceberBin(void);
-
 int TransformarBinToDec(int bin);
-
 float RealizarOperacao(int decimal1, int decimal2);
-
 int Retransformar(int resultado);
-
-
-//...............................................................
+//.....................................................................        
 int main(void)
 {
   int bin = 0;
   int dec1 = 0, dec2 = 0;
   int resultado = 0;
-//...............................................................
-  PedirDados();  
-  //ReceberDados();
-    scanf("%d", &bin);
-      dec1 = TransformarBinToDec(bin);
-  PedirOperador();
-    scanf("%s", operador);
-  PedirDados();
-    scanf("%d", &bin);
-      dec2 = TransformarBinToDec(bin);
-//...............................................................
-//printf("Dec1 = %d\nDec2 = %d\nOperador = %s\n", dec1, dec2, operador);
+  printf("Digite o caracter do operador(+,-,*,/ ou porcentagem):\n");
+  scanf("%s", operador);
+  printf("Digite um numero binario de ate 8 bits (entre 0 e 255):\n");
+  scanf("%d", &bin);
+  dec1 = TransformarBinToDec(bin);
+  printf("Digite um segundo numero binario de ate 8 bits (entre 0 e 255):\n");
+  scanf("%d", &bin);
+  dec2 = TransformarBinToDec(bin);
   resultado = RealizarOperacao(dec1, dec2);
-  //printf("deu: %d", resultado);
-  //printf("O resultado da operação foi: %d", resultado);
-  printf("O Resultado da operação foi: ");
+  printf("O resultado da sua operacao foi: ");
   Retransformar(resultado);
-  
   return 0;
 }
-
-//.....................................................................
-
-void PedirDados(void)
-{
-    //1- pedir a operação e o cod binario(printf);
-  printf("Digite o numero binario:\n");
-}
-
-void PedirOperador(void)
-{
-    //1- pedir a operação e o cod binario(printf);
-  printf("Digite o operador:\n");
-}
-    
+//.....................................................................        
 int TransformarBinToDec(int bin)
 {
   int total  = 0;
   int potenc = 1;
-
   while(bin > 0) 
   {
     total += bin % 10 * potenc;
     bin    = bin / 10;
     potenc = potenc * 2;
   }
-
   return total;
 }
-
+//.....................................................................        
 float RealizarOperacao(int decimal1, int decimal2)
 {
   float resultado = 0;
-    //4- fazer a operação;
   if(strcmp(operador,"+") == 0)
   {
-  //printf("vc escolheu +\n"); 
   resultado = decimal1 + decimal2;
   }
-  
   if(strcmp(operador,"*") == 0)
   {
     resultado = decimal1 * decimal2;
   }
-
   if(strcmp(operador,"/") == 0)
   {
     resultado = decimal1 / decimal2;
   }
-
   if(strcmp(operador,"-") == 0)
   {
     resultado = decimal1 - decimal2;
   }
-
   if(strcmp(operador,"%") == 0)
   {
     resultado = decimal1 % decimal2;  
   }
-  
-  //printf("o resultado foi: %.2f", resultado);
   return(resultado);
 }
-
+//.....................................................................        
 int Retransformar(int resultado)
 {
-  int resposta = 0;
-  int vet_bin[256];
-  int i = 0, j;
-  //5- trasnformar de volta em binario;
-
-  while(resultado>0)
+  int vet_bin[8];
+  int i = 0;
+  while(i<8)
+  {
+    vet_bin[i] = resultado % 2;
+    resultado = resultado/2;
+    i++;
+  }
+  while (i>0)
+  {
+    if (vet_bin[i-1] == 1)
     {
-      vet_bin[i] = resultado % 2;
-      i++;
-      resultado = resultado/2;
+     printf("1");
     }
-  for(j = i - 1 ; j >= 0; j--)
-    { 
-      printf("%d", vet_bin[j]);
-     // printf("\n");
+    else
+    {
+     printf("0");
     }
-  //return(reposta);
+    i--;
+  }
   return 0;
 }
+//.....................................................................    
